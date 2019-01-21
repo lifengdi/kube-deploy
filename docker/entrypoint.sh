5201b1267a42b5689c11b97b2ef2d3a33f6103ff
liukai <liu-kevin@foxmail.com>
2019-01-21 10:24:53 +0800

    edit entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,4 +2,11 @@
 
 echo -e "执行go-script"
 
+
+go get -u github.com/kardianos/govendor
+
+govendor init
+
+govendor list +missing |awk '{print $2}'|xargs go get
+
 go run /go/src/kube-deploy/web/main.go
